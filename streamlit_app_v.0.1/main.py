607e195eac7122a6dc45b5f03b4c6adf25ac6c2c
Pedro-goener <pedro.coelho@goener.com.br>
2025-01-06 09:04:19 -0300

    teste biblioteca path

--- streamlit_app_v.0.1/main.py
+++ streamlit_app_v.0.1/main.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from PIL import Image
-icon = '.streamlit/Logo_azul_quadrada.PNG'
-st.set_page_config(page_title="Goener", layout="wide",page_icon=icon)
+from pathlib import Path
+icon_path = Path('.streamlit/Logo_azul_quadrada.PNG')
+img_path = Path(".streamlit/Logo_goener_colorida.png")
+#icon = '.streamlit/Logo_azul_quadrada.PNG'
+st.set_page_config(page_title="Goener", layout="wide",page_icon=icon_path)
 # Carregar a imagem com Pillow
-img = Image.open('.streamlit/Logo_goener_colorida.png')
+img = Image.open(img_path)
 
 # Redimensionar a imagem (alterando a altura e mantendo a proporção)
 img = img.resize((int(img.width * (50 / img.height)), 50))  # Altura = 30 pixels
