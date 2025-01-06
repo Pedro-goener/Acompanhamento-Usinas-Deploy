5d82e26d2ca919bbeda5d64d897453c16c5f320e
Pedro-goener <pedro.coelho@goener.com.br>
2025-01-06 08:55:23 -0300

    Commit com teste de arquivo imagem

--- streamlit_app_v.0.1/main.py
+++ streamlit_app_v.0.1/main.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from PIL import Image
-icon = '.streamlit/Logo_azul_quadrada.PNG'
-st.set_page_config(page_title="Goener", layout="wide",page_icon=icon)
-# Carregar a imagem com Pillow
-img = Image.open('.streamlit/Logo_goener_colorida.png')
-
-# Redimensionar a imagem (alterando a altura e mantendo a proporção)
-img = img.resize((int(img.width * (50 / img.height)), 50))  # Altura = 30 pixels
-
-# Exibir a imagem redimensionada
-st.image(img)
+# icon = '.streamlit/Logo_azul_quadrada.PNG'
+# st.set_page_config(page_title="Goener", layout="wide",page_icon=icon)
+# # Carregar a imagem com Pillow
+# img = Image.open('.streamlit/Logo_goener_colorida.png')
+#
+# # Redimensionar a imagem (alterando a altura e mantendo a proporção)
+# img = img.resize((int(img.width * (50 / img.height)), 50))  # Altura = 30 pixels
+#
+# # Exibir a imagem redimensionada
+# st.image(img)
 st.title("Análise de Ocorrência das Usinas")
 
 
