46e2ebb597fde21397e996e5b98b6a9e493ec81f
Pedro-goener <pedro.coelho@goener.com.br>
2025-01-06 09:08:17 -0300

    teste os

--- streamlit_app_v.0.1/main.py
+++ streamlit_app_v.0.1/main.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from PIL import Image
-from pathlib import Path
-icon_path = Path('.streamlit/Logo_azul_quadrada.PNG')
-img_path = Path(".streamlit/Logo_goener_colorida.png")
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+icon_path = os.path.join(current_dir,'.streamlit','Logo_azul_quadrada.PNG')
+img_path = os.path.join(current_dir,'.streamlit','Logo_goener_colorida.png')
 #icon = '.streamlit/Logo_azul_quadrada.PNG'
 st.set_page_config(page_title="Goener", layout="wide",page_icon=icon_path)
 # Carregar a imagem com Pillow
