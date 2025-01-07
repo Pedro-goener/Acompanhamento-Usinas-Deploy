0a39c92f9193231e81a4bc500ec0427778ac60af
Pedro-goener <pedro.coelho@goener.com.br>
2025-01-07 13:38:48 -0300

    Adição de configurações via CSS

--- streamlit_app_v.0.1/main.py
+++ streamlit_app_v.0.1/main.py
@@ -8,6 +8,34 @@ icon_path = os.path.join(current_dir,'.streamlit','Logo_azul_quadrada.PNG')
 #Achando o caminho da logo
 img_path = os.path.join(current_dir,'.streamlit','Logo_goener_colorida.png')
 st.set_page_config(page_title="Goener", layout="wide",page_icon=icon_path)
+# Aplicar o tema via CSS
+st.markdown("""
+    <style>
+        :root {
+            --primary-color: #6A994E;
+            --background-color: #F4F4F9;
+            --secondary-background-color: #A7C957;
+            --text-color: #386641;
+        }
+
+        .stApp {
+            background-color: var(--background-color) !important;
+        }
+
+        .stSidebar .sidebar-content {
+            background-color: var(--secondary-background-color) !important;
+        }
+
+        p, .stMarkdown {
+            color: var(--text-color) !important;
+        }
+
+        .stButton>button {
+            color: white !important;
+            background-color: var(--primary-color) !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
 # Carregar a imagem com Pillow
 img = Image.open(img_path)
 
